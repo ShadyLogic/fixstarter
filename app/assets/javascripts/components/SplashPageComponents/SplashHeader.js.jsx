18a3af63ef4dbd690f17c2fec608ce77039e5b3c
Tim Cannady <timcannady@gmail.com>
2015-08-08 12:05:43 -0700

    add functioning framework for register_login component. I'm currently using the welcome_dashboard path, which smells. Not seeing a register controller of sorts

--- app/assets/javascripts/components/SplashPageComponents/SplashHeader.js.jsx
+++ app/assets/javascripts/components/SplashPageComponents/SplashHeader.js.jsx
@@ -6,7 +6,7 @@ var SplashHeader = React.createClass({
       <div className="header_wrapper">
         <h1> FIXSTART </h1>
         <h3> Where you can fix all the shits </h3>
-        <a href="/welcome/authentication"> Get Started </a>
+        <a href="/dashboard"> Get Started </a>
       </div>
     )
   }
