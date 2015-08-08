1a5a18ac1a9ebbdd6144c40f97249a407e37c3cb
Tim Cannady <timcannady@gmail.com>
2015-08-08 11:41:04 -0700

    mild css

--- app/assets/javascripts/components/SplashPageComponents/SplashPage.js.jsx
+++ app/assets/javascripts/components/SplashPageComponents/SplashPage.js.jsx
@@ -2,7 +2,7 @@ var SplashPage = React.createClass({
 
   render: function(){
     return (
-      <div>
+      <div className = "splash_page">
         < SplashHeader />
         < br/>
         < Stream streamIssues={this.props.streamIssues} />
