cf659267db7c75cbbb00cafe05ce76f813b3054d
Andrea Coravos <acoravos@gmail.com>
2015-08-08 16:08:02 -0700

    Add user profile and user title as children within the user header component

--- app/assets/javascripts/components/UserShowComponents/UserHeader.js.jsx
+++ app/assets/javascripts/components/UserShowComponents/UserHeader.js.jsx
@@ -2,7 +2,8 @@ var UserHeader = React.createClass({
   render: function(){
     return (
       <div className="user_header_wrapper">
-      <p> This is the user header.</p>
+      < ProfilePic />
+      < UserTitle />
       </div>
       )
   }
