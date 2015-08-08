3aeff524af0783f38b57b955a174c8ecbb101cfd
Tim Cannady <timcannady@gmail.com>
2015-08-08 13:32:53 -0700

    change register_login prefix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,19 +1,23 @@
 class WelcomeController < ApplicationController
+
   def index
     @stream_issues = Issue.package_stream_issues
   end
 
+
   def show
-     @stream_issues = Issue.package_stream_issues
+    @stream_issues = Issue.package_stream_issues
     if current_user
       render 'welcome/show'
     else
-      redirect_to registerlogin_path
+      redirect_to register_login_path
     end
   end
 
+
   def discover
   end
+
 
   def register_login
     @stream_issues = Issue.package_stream_issues
