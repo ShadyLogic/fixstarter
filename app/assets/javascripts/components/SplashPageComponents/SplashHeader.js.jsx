c16cce5313e8b7a6a387b5b06d884d0dde6ca6a2
Tim Cannady <timcannady@gmail.com>
2015-08-08 10:53:46 -0700

    fix typo in header tag

--- app/assets/javascripts/components/SplashPageComponents/SplashHeader.js.jsx
+++ app/assets/javascripts/components/SplashPageComponents/SplashHeader.js.jsx
@@ -1,9 +1,14 @@
-// var SplashHeader = React.createClass({
-//   render: function(){
-//     <div>
-//       <h1>Fixstarter</h2>
-//       <h3>Fuck yeah fixstarter</h3>
-//       <a href="/welcome/authentication">Get Started</a>
-//     </div>
-//   }
-// })
+var SplashHeader = React.createClass({
+
+  render: function(){
+
+    return (
+      <div>
+        // <h1>Fixstarter</h1>
+        fuck yeah splash header!
+        // <h3>Fuck yeah fixstarter</h3>
+        // <a href="/welcome/authentication">Get Started</a>
+      </div>
+    )
+  }
+})
