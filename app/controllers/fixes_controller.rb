aaddcc3e1ee60362d39bd6954477b1608663d620
Tim Cannady <timcannady@gmail.com>
2015-08-09 15:36:52 -0700

    add logic so Current_User works.

--- app/controllers/fixes_controller.rb
+++ app/controllers/fixes_controller.rb
@@ -1,5 +1,8 @@
 class FixesController < ApplicationController
 
+  # Allows for json post requests to process without the need for a CSRF
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+
   def show
     if @fix = Fix.find_by(id: params[:id])
       @issue = @fix.issue
@@ -16,9 +19,7 @@ class FixesController < ApplicationController
   def create
     issue = Issue.find(params[:issue_id])
     @fix = Fix.new(fix_params)
-    # p "************"
-    # p current_user
-    # current_user.fixes << @fix
+    current_user.fixes << @fix
     if @fix.save
       issue.status = "closed"
       issue.save
