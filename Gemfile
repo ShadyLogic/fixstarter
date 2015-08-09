b5d9d955919b2022fa035730cf80541eecf06b11
Jacob Rogers <JacobRogers42@gmail.com>
2015-08-09 00:36:43 -0700

    Added bogus image files

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'pg'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'devise'
+gem 'httparty'
 
 
 gem 'jquery-rails'
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -22,4 +22,27 @@ class WelcomeController < ApplicationController
     @stream_issues = Issue.package_stream_issues
   end
 
+  def image
+    p "*"*100
+    p params
+    p "*"*100
+    p params[:image].tempfile.open
+    p "*"*100
+
+    file = params[:image].tempfile.open
+
+    query = { 'image' => params[:image].tempfile.open }
+    headers = { "Authorization" => "Client-ID 3deb91dad34b579"}
+
+    response = HTTParty.post('https://api.imgur.com/3/image',
+                        :query => query,
+                        :headers => headers)
+
+    p "*"*100
+    p response
+    p "*"*100
+
+    redirect_to user_path(1)
+  end
+
 end
