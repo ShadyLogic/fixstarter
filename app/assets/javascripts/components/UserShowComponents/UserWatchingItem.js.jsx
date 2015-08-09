4ab1f49bfbe5d3ae6f82c11f69618154dcef2524
Andrea Coravos <acoravos@gmail.com>
2015-08-09 14:31:23 -0700

    Add watches to the user show page / profile page

--- app/assets/javascripts/components/UserShowComponents/UserWatchingItem.js.jsx
+++ app/assets/javascripts/components/UserShowComponents/UserWatchingItem.js.jsx
@@ -2,7 +2,7 @@ var UserWatchingItem = React.createClass({
   render: function(){
     return (
       <div className="user_watching_item_wrapper">
-      <p> This is a list of issues being watched by the user</p>
+      <p> {this.props.issue_id} </p>
       </div>
       )
   }
--- app/assets/javascripts/components/UserShowComponents/UserWatchingList.js.jsx
+++ app/assets/javascripts/components/UserShowComponents/UserWatchingList.js.jsx
@@ -1,9 +1,14 @@
 var UserWatchingList = React.createClass({
   render: function(){
+
+    var Watches = this.props.watches.map(function(watch) {
+      return < UserWatchingItem key={watch.id} issue_id={watch.issue_id} />
+    });
+
     return (
       <div className="user_watching_list_wrapper">
       <h2> This is a list of items you are watching. </h2>
-      < UserWatchingItem watches={this.props.watches} / >
+      {Watches}
       </div>
       )
   }
