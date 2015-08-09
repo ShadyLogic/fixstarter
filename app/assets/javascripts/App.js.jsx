8b9775ccb297357d3338119cd6cd0bfcd80ec02a
Tim Cannady <timcannady@gmail.com>
2015-08-08 19:15:14 -0700

    comment out stuff

--- app/assets/javascripts/App.js.jsx
+++ app/assets/javascripts/App.js.jsx
@@ -19,13 +19,13 @@ App.request = function(method, path, data){
   });
 };
 
-App.goto = function(path){
-  history.pushState({}, document.title, path);
-  $(window).trigger('pushstate');
-};
+// App.goto = function(path){
+//   history.pushState({}, document.title, path);
+//   $(window).trigger('pushstate');
+// };
 
-App.router = function(path){
-  if (path === '/')                         return <HomePage />;
-  if (path.match(/^\/questions\/(\d+)\/comments$/))   return <CommentShowPage  question_id={RegExp.$1} />;
-  return <h1>Page Not Found</h1>
-};
+// App.router = function(path){
+//   if (path === '/')                         return <HomePage />;
+//   if (path.match(/^\/questions\/(\d+)\/comments$/))   return <CommentShowPage  question_id={RegExp.$1} />;
+//   return <h1>Page Not Found</h1>
+// };
