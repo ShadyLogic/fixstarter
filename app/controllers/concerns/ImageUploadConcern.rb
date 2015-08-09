028e6f876d29817f59e126ab6fd873d489465fba
Jacob Rogers <JacobRogers42@gmail.com>
2015-08-09 15:31:28 -0700

    Break out image upload function to ImageUploadConcern

---
+++ app/controllers/concerns/ImageUploadConcern.rb
@@ -0,0 +1,19 @@
+module ImageUploadConcern
+
+  extend ActiveSupport::Concern
+
+  def upload_image
+    file = Base64.encode64(open(params[:image].tempfile) { |io| io.read })
+
+    body = { 'image' => file }
+    headers = { "Authorization" => "Client-ID 3deb91dad34b579" }
+
+    response = HTTParty.post('https://api.imgur.com/3/image',
+                        :body => body,
+                        :headers => headers)
+
+
+    image_url = response["data"]["link"]
+
+    return image_url
+  end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -29,22 +29,6 @@ class WelcomeController < ApplicationController
     p params[:image].tempfile
     p "*"*100
 
-    file = Base64.encode64(open(params[:image].tempfile) { |io| io.read })
-
-    body = { 'image' => file }
-    headers = { "Authorization" => "Client-ID 3deb91dad34b579"}
-
-    response = HTTParty.post('https://api.imgur.com/3/image',
-                        :body => body,
-                        :headers => headers)
-
-    p "RESPONSE: #{response}"
-    p "*"*100
-
-    image_url = response["data"]["link"]
-
-    p image_url
-
     redirect_to image_url
   end
 
