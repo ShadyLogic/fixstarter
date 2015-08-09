98ceab73f561541c6d6febefd00499dcb7a0fb31
Jacob Rogers <JacobRogers42@gmail.com>
2015-08-08 19:30:55 -0700

    Implement fixes#show page! Fixes now recieve and display appropriate data

--- app/assets/javascripts/components/SharedComponents/MediaCard.js.jsx
+++ app/assets/javascripts/components/SharedComponents/MediaCard.js.jsx
@@ -2,7 +2,7 @@ var MediaCard = React.createClass({
   render: function(){
     return (
       <div className="media_card">
-        <h3> Media Card </h3>
+        <h4> Media Card </h4>
         < MediaImage image_url={this.props.image_url} />
         < MediaText description={this.props.description} />
       </div>
--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,9 +1,12 @@
 class IssuesController < ApplicationController
 
   def show
-    @issue = Issue.find_by(id: params[:id])
-    @fixes = @issue.fixes
-    @comments = @issue.issue_comments.map { |comment| comment.package_info }
+    if @issue = Issue.find_by(id: params[:id])
+      @fixes = @issue.fixes
+      @comments = @issue.issue_comments.map { |comment| comment.package_info }
+    else
+      redirect_to welcome_index_path
+    end
   end
 
   def new
