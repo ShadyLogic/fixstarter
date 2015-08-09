b03b230eda224dce2d7281661ff24ce9c26f14b3
Jacob Rogers <JacobRogers42@gmail.com>
2015-08-08 17:21:38 -0700

    Issues#Show is working! All content is being pulled correctly, just needs some CSS.

--- app/assets/javascripts/components/SharedComponents/CommentsList.js.jsx
+++ app/assets/javascripts/components/SharedComponents/CommentsList.js.jsx
@@ -1,9 +1,13 @@
 var CommentsList = React.createClass({
   render: function(){
+    var Comments = this.props.comments.map(function(comment) {
+      return < Comment key={comment.id} content={comment.content} user={comment.user} />
+    });
+
     return (
       <div className="comments_list">
-        <h3> Comments List </h3>
-        < Comment />
+        <h3> Comments: </h3>
+        {Comments}
       </div>
       )
   }
