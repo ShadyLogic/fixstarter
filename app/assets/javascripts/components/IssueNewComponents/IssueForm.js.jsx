01c606116470446464752ef9f22c6520349f09d1
Tim Cannady <timcannady@gmail.com>
2015-08-08 19:10:10 -0700

    add form framework to Issue_new

--- app/assets/javascripts/components/IssueNewComponents/IssueForm.js.jsx
+++ app/assets/javascripts/components/IssueNewComponents/IssueForm.js.jsx
@@ -2,7 +2,20 @@ var IssueForm = React.createClass({
   render: function(){
     return (
       <div className="issue_form_wrapper">
-        Issue form
+        <form>
+          Title
+          <input type="text" />
+          <br/>
+          Description
+          <input type="text" />
+          <br/>
+          Zip Code:
+          <input type="text" />
+          <br/>
+          Image:
+          <input type="file" />
+          <input type="submit" />
+        </form>
       </div>
     )
   }
