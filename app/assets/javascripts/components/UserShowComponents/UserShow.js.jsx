cd8e1e32adc7a7f04997cf9cdf7374e5bab5a093
Andrea Coravos <acoravos@gmail.com>
2015-08-09 21:15:02 -0700

    Add functionality to alter the view of looking at one's own profile vs another user's profile

--- app/assets/javascripts/components/UserShowComponents/UserShow.js.jsx
+++ app/assets/javascripts/components/UserShowComponents/UserShow.js.jsx
@@ -2,6 +2,13 @@ var UserShow = React.createClass({
   render: function(){
     return (
       <div className="user_show_wrapper">
+
+      {this.props.same_user ?
+        <p> You are viewing your own profile </p>
+      :
+        <p> You are viewing someone elses profile. </p>
+      }
+
       <h1> Hello! Welcome to your FixStart profile page. </h1>
       < UserHeader user={this.props.user} />
       < UserBio user={this.props.user} />
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,13 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
+
+    @same_user = false
+
+    if @user.id == current_user.id
+      @same_user = true
+    end
+
     @issues = @user.issues.map { |issue| issue.package_info}
     @fixes = @user.fixes.map { |fix| fix.package_info}
     @watches = @user.issues_watches.map { |watches| watches.package_info}
