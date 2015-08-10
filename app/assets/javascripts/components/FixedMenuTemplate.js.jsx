0841b23cd6eec32a06186b771f28167b8338f7c3
Jacob Rogers <JacobRogers42@gmail.com>
2015-08-09 19:10:43 -0700

    Add logout button

--- app/assets/javascripts/components/FixedMenuTemplate.js.jsx
+++ app/assets/javascripts/components/FixedMenuTemplate.js.jsx
@@ -6,10 +6,7 @@ var FixedMenuTemplate = React.createClass({
       <p> <a href="/issues/new"> Submit a New Issue </a> </p>
       <p> <a href="/dashboard"> Dashboard </a> </p>
       <p> <a href="/discover"> Discover </a> </p>
-      <form className="log-out-link" action="/users/sign_out" method="post">
-        <input type="hidden" name="_method" value="delete" />
-        <button type="submit">Log Out</button>
-      </form>
+      <a href="/users/sign_out" data-method="delete" rel="nofollow">Logout</a>
       </div>
       )
   }
