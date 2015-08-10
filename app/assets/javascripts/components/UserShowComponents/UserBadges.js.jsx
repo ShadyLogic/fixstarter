dd09187f1b4f08b21d1a9a1fdb7f20f7fdb1788b
Andrea Coravos <acoravos@gmail.com>
2015-08-09 17:46:29 -0700

    Add UserBadges component

---
+++ app/assets/javascripts/components/UserShowComponents/UserBadges.js.jsx
@@ -0,0 +1,9 @@
+var UserBadges = React.createClass({
+  render: function(){
+    return (
+      <div className="user_bio_wrapper">
+      <p> These are the user badges. </p>
+      </div>
+      )
+  }
+})
