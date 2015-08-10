ea8d1f71d5fc8514b29f295beb54a461107779d8
mwhatters <mwhatters@gmail.com>
2015-08-10 00:19:55 -0700

    add current location ability to issue new page

--- app/assets/javascripts/components/IssueNewComponents/IssueMap.js.jsx
+++ app/assets/javascripts/components/IssueNewComponents/IssueMap.js.jsx
@@ -2,17 +2,18 @@ var IssueMap = React.createClass({
 
 	componentDidMount: function() {
 		this.renderMap();
+		this.findCurrentLocation();
 				// map needs acceess to other functions
 	},
 
 	renderMap: function() {
 		L.mapbox.accessToken = 'pk.eyJ1IjoibXdoYXR0ZXJzIiwiYSI6ImM5YjljNGE4MzcwZmRlOTJlOTNmMTczMTY4N2FkMDNiIn0.mYeJTMzrCEY8XMUqqTp6tg';
 		var geocoder 		 = L.mapbox.geocoder('mapbox.places'),
-		    latestMarker = undefined,
 		    self 				 = this
 
 		//map variable shared between functions.
 		map = L.mapbox.map('map', 'mapbox.streets')
+		latestMarker = undefined
 
 		 map.on('click', function(e) {
 				if (latestMarker === undefined) {} else {
@@ -23,20 +24,16 @@ var IssueMap = React.createClass({
 
 		    self.props.latValue = e.latlng.lat
 		    self.props.lonValue = e.latlng.lng
-		    console.log(self.props)
 		  });
 
 			map.on('mouseup', function(e) {
 				self.props.latValue = e.latlng.lat
 				self.props.lonValue = e.latlng.lng
-		    console.log(self.props)
 			})
 
 		//show the users zip code area of the map
 		geocoder.query(this.props.zip, self.showMap)
 	},
-
-
 
 	addMarker: function(c1, c2) {
 		var marker = L.marker(new L.LatLng(c1, c2), {
@@ -57,10 +54,40 @@ var IssueMap = React.createClass({
     } 
 	},
 
+	// --------------- CURRENT LOCATION RENDERING ------------------ //
+
+	findCurrentLocation: function() {
+		var geolocate = $('#geoLocate')
+		var self = this
+    geolocate.click(function (e) {
+    	debugger
+        e.preventDefault();
+        e.stopPropagation();
+        map.locate();
+        self.setMarkerAtCurrentLocation();
+    });
+
+	},
+
+	setMarkerAtCurrentLocation: function() {
+		var self = this
+		map.on('locationfound', function(e) {
+				if (latestMarker === undefined) {} else {
+					this.removeLayer(latestMarker) }
+
+		    map.fitBounds(e.bounds);
+		    latestMarker = self.addMarker(e.latlng.lat, e.latlng.lng)
+		    latestMarker.addTo(map)
+		   	self.props.latValue = e.latlng.lat
+		    self.props.lonValue = e.latlng.lng
+		});
+	},
+
   render: function() {
     return (
       <div className="issue_map_wrapper">
         <div id='map'></div>
+        <button id='geoLocate'>Find current location</button>
       </div>
     )
   }
