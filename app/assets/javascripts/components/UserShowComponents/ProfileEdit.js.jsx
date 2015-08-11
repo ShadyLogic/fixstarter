10b79af8a878b8a1a0c3133a10e6909835e819d1
Andrea Coravos <acoravos@gmail.com>
2015-08-11 16:10:07 -0700

    Update profile form for given user

--- app/assets/javascripts/components/UserShowComponents/ProfileEdit.js.jsx
+++ app/assets/javascripts/components/UserShowComponents/ProfileEdit.js.jsx
@@ -19,32 +19,39 @@ var ProfileEdit = React.createClass({
       <p> Hello, {this.props.user.first_name} {this.props.user.last_name}.
       You can update your profile below. </p>
 
-      <form action='/profile' method="POST"  ref="form" encType="multipart/form-data">
+      <form className="ui form" action='/profile' method="POST"  ref="form" encType="multipart/form-data">
           <input type="hidden" name="_method" value="PUT" />
 
-          First Name<br/>
+          <div className="field">
+          <label>First name</label>
           <input type="text" name="user[first_name]" valueLink={this.linkState("first_name")} />
-          <br/>
+          </div>
 
-          Last Name<br/>
+          <div className="field">
+          <label>Last name</label>
           <input type="text" name="user[last_name]" valueLink={this.linkState("last_name")} />
-          <br/>
+          </div>
 
-          Email<br/>
+          <div className="field">
+          <label>Email</label>
           <input type="text" name="user[email]" valueLink={this.linkState("email")}/>
-          <br/>
+          </div>
 
-          Bio<br/>
-          <textarea type="comment" name="user[bio]" valueLink={this.linkState("bio")}/>
-          <br/>
+          <div className="field">
+          <label>Bio</label>
+          <textarea rows="2" type="comment" name="user[bio]" valueLink={this.linkState("bio")}/>
+          </div>
 
-          Upload a new Avatar<br/>
+          <div className="field">
+          <label>Upload a new Avatar</label>
           < ImageUploadForm />
-          <br/><br/>
+          </div>
 
-          <button type="submit">Update Profile</button>
+          <button className="ui primary button" type="submit">Update Profile</button>
 
         </form>
+
+        <br></br>
 
       </div>
       )
