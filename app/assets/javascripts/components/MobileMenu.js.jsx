27d8cb1637f42cb062fec792824eb547c8542e96
Tim Cannady <timcannady@gmail.com>
2015-08-12 13:41:06 -0700

    add framework for mobile menu

---
+++ app/assets/javascripts/components/MobileMenu.js.jsx
@@ -0,0 +1,10 @@
+var MobileMenu = React.createClass({
+  render: function(){
+    return (
+      <div className="ui labeled icon button black">
+        <i className="icon list layout"></i>
+          Menu
+      </div>
+    )
+  }
+})
