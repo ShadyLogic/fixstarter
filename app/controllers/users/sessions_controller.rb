221181062c829f5f1d93597d76b04d98c0ed6437
Tim Cannady <timcannady@gmail.com>
2015-08-12 10:25:54 -0700

    sign up and log in pages now have basic splash'esque background

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,9 +2,10 @@ class Users::SessionsController < Devise::SessionsController
 # before_filter :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    @log_in_page = true
+    super
+  end
 
   # POST /resource/sign_in
   # def create
