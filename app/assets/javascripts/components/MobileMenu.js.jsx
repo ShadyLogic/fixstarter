d95151daf39fd6bcc0095d3fd8aab93f2f652dec
Tim Cannady <timcannady@gmail.com>
2015-08-12 14:24:44 -0700

    sidebar

--- app/assets/javascripts/components/MobileMenu.js.jsx
+++ app/assets/javascripts/components/MobileMenu.js.jsx
@@ -1,7 +1,7 @@
 var MobileMenu = React.createClass({
   render: function(){
     return (
-      <div className="ui labeled icon button black">
+      <div id="m_btn" className="ui labeled icon button black">
         <i className="icon list layout"></i>
           Menu
       </div>
