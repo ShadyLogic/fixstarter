1717eaf4b0fda43f839a63095a30cf24a8e3d093
Andrea Coravos <acoravos@gmail.com>
2015-08-12 13:58:10 -0700

    Update categories with icons and add content for community standards and about us

--- app/assets/javascripts/components/AboutTeamComponents/AboutTeamShow.js.jsx
+++ app/assets/javascripts/components/AboutTeamComponents/AboutTeamShow.js.jsx
@@ -2,9 +2,9 @@ var AboutTeamShow = React.createClass({
   render: function(){
     return (
       <div className="about_team_show_page">
+        < TeamMediaCards />
         < CommunityStandards />
         < CategoryDescriptions />
-        < TeamMediaCards />
       </div>
       )
   }
--- app/assets/javascripts/components/AboutTeamComponents/CommunityStandards.js.jsx
+++ app/assets/javascripts/components/AboutTeamComponents/CommunityStandards.js.jsx
@@ -3,6 +3,10 @@ var CommunityStandards = React.createClass({
     return (
       <div className="about_team_show_page">
         <h4 className="ui horizontal divider header"> Community Standards </h4>
+          <p> Issues submitted to FixStart should be only issues that other members of the civilian community can and/or should fix. </p>
+          <p> Any issue that is beyond the basic repair of a fellow civilian should be <b>escalated</b> to your <a href="http://sf311.org/index.aspx?page=87">local city</a>. </p>
+        <br/>
+
       </div>
       )
   }
