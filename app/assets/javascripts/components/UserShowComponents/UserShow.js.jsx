59fa84fff1f5a4a7a214d835a6e55ec4a8d81e6b
Andrea Coravos <acoravos@gmail.com>
2015-08-12 18:26:51 -0700

    Update user show page to center and remove white space

--- app/assets/javascripts/components/UserShowComponents/UserShow.js.jsx
+++ app/assets/javascripts/components/UserShowComponents/UserShow.js.jsx
@@ -3,18 +3,23 @@ var UserShow = React.createClass({
     return (
       <div className="user_show_wrapper">
 
+      <br/>
 
-      <h3 className="ui horizontal divider header"> About Me </h3>
+        <div className="ui stackable three column center aligned grid">
+          <div className="column">
+          <h3 className="ui horizontal divider header"> About Me </h3>
 
-      < UserProfileCard user={this.props.user} same_user={this.props.same_user} />
+          < UserProfileCard user={this.props.user} same_user={this.props.same_user} />
 
-      <br></br>
+          <br></br>
 
-      < Badges badges={this.props.badges} />
+          < Badges badges={this.props.badges} />
+          </div>
 
-      <br></br>
-
-      < UserActivity user={this.props.user} issues={this.props.issues} fixes={this.props.fixes} watches={this.props.watches} same_user={this.props.same_user} />
+          <div className=" column">
+          < UserActivity user={this.props.user} issues={this.props.issues} fixes={this.props.fixes} watches={this.props.watches} same_user={this.props.same_user} />
+          </div>
+        </div>
 
       </div>
       )
