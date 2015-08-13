b4a89782b46ebc88e7ccdf2c8ed12480a7798a9e
Jacob Rogers <JacobRogers42@gmail.com>
2015-08-13 15:25:35 -0700

    Fix password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable
   has_many :issues_watches
   has_many :fix_comments
   has_many :fixes
