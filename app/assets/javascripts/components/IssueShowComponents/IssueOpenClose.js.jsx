0286dca70ac8bb32c79ae66a6e8642d31d104779
Jacob Rogers <JacobRogers42@gmail.com>
2015-08-12 20:56:19 -0700

    AJAXIFY open/close issue button

--- app/assets/javascripts/components/IssueShowComponents/IssueOpenClose.js.jsx
+++ app/assets/javascripts/components/IssueShowComponents/IssueOpenClose.js.jsx
@@ -1,29 +1,66 @@
 var IssueOpenClose = React.createClass({
+
+  getInitialState: function(){
+    if (this.props.issue.status == "closed"){
+      var path = "/issues/"+this.props.issue.id+"/reopen"
+      var button = ""
+      var button_text = "Re-open Issue"
+      var message = "You created this issue, and someone has submitted a fix, which auto-closed your issue. Would you like to re-open the issue?"
+      var status = "closed"
+    }else{
+      var path= "/issues/"+this.props.issue.id+"/close"
+      var button = " red"
+      var button_text = "Close Issue"
+      var message = "You created this issue, but no one has submitted fixes yet, would you like to close your issue?"
+      var status = "open"
+    }
+    return {
+      message: message,
+      button: button,
+      button_text: button_text,
+      status: status,
+      path: path
+    }
+
+  },
+
+  handleSubmit: function(e){
+    e.preventDefault();
+
+    App.jacobs_request("PUT", this.state.path)
+
+    if (this.state.status == "closed"){
+      this.state.path= "/issues/"+this.props.issue.id+"/close"
+      this.state.status = "open"
+      this.state.button = " red"
+      this.state.button_text = "Close Issue"
+      this.state.message = "You created this issue, but no one has submitted fixes yet, would you like to close your issue?"
+    }else{
+      this.state.path= "/issues/"+this.props.issue.id+"/reopen"
+      this.state.status = "closed"
+      this.state.button = ""
+      this.state.button_text = "Re-open Issue"
+      this.state.message = "You created this issue, and someone has submitted a fix, which auto-closed your issue. Would you like to re-open the issue?"
+    }
+
+    this.forceUpdate()
+
+  },
+
   render: function(){
     return (
       <div className="issue_open_close">
 
       <h3 className="ui horizontal divider header"> My Stats </h3>
 
-      <p> {this.props.issue.status == "closed" ?
-          <p> You created this issue, and someone has submitted a fix, which auto-closed your issue. Would you like to re-open the issue?
+      <p>
+          <p> {this.state.message}
 
-                <form action={"/issues/"+this.props.issue.id+"/reopen"} method="POST">
-                <input type="hidden" name="_method" value="PUT" />
-                <button className="ui button" type= "Submit"> Re-open Issue </button>
+                <form onSubmit={this.handleSubmit}>
+                <button className={"ui button"+this.state.button} type= "Submit"> {this.state.button_text} </button>
                 </form>
 
           </p>
-        :
-        <p> You created this issue, but no one has submitted fixes yet, would you like to close your issue?
-
-              <form action={"/issues/"+this.props.issue.id+"/close"} method="POST">
-              <input type="hidden" name="_method" value="PUT" />
-              <button className="ui button" type= "Submit"> Close the issue </button>
-              </form>
-
-        </p>
-      }
       </p>
 
       </div>
