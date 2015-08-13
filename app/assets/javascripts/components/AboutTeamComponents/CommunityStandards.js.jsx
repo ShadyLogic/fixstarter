dfd82b64caca38a4b197dec5ae064ae8ec7362d5
mwhatters <mwhatters@gmail.com>
2015-08-13 10:55:34 -0700

    community standards verbage

--- app/assets/javascripts/components/AboutTeamComponents/CommunityStandards.js.jsx
+++ app/assets/javascripts/components/AboutTeamComponents/CommunityStandards.js.jsx
@@ -3,6 +3,9 @@ var CommunityStandards = React.createClass({
     return (
       <div className="about_team_show_page">
         <h2 className="ui horizontal divider header"> Community Standards </h2>
+          <p> Here at FixStart, we believe that a community thrives only as much as its people.</p>
+          <p> FixStart is an experiment that asks the question of how we can get people talking about and actively participating in their communities and neighborhoods. See something that could be fixed or touched up, but don't have the tools yourself to make it happen? Submit an issue. Let your community know. Have a fellow good samaritanm appropriately-equipped for the job, take on the task.</p>
+          <p> Conversely, engage in resolving locally submitted issues by taking them on yourself. See that you have the tools to fix an issue? Hop on it, and get rewarded for your efforts with FixStart badges.</p>
           <p> Issues submitted to FixStart should be only issues that other members of the civilian community can and/or should fix. </p>
           <p> Any issue that is beyond the basic repair of a fellow civilian should be <b>escalated</b> to your <a href="http://sf311.org/index.aspx?page=87">local city</a>. </p>
         <br/>
